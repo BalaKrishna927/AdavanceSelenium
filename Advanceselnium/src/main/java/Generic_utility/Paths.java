a8cdb01f11d4e969aac434a4f4ff801b35c73bec
BalaKrishna927 <malito:kbalakrishna9666@gmail.com>
2025-01-06 08:06:06 +0530

    Branch1

--- Advanceselnium/src/main/java/Generic_utility/Paths.java
+++
@@ -1,9 +0,0 @@
-package Generic_utility;
-
-public interface Paths {
-	
- String filepath  =	"./src/test/resources/Propertiefile1.txt";
- 
- String Ecxelpath  =	"./src/test/resources/DataExcel.xlsx";
-
-}
--- Advanceselnium/src/main/java/Pom_Repos/FindAll.java
+++
@@ -1,9 +0,0 @@
-package Pom_Repos;
-
-import org.openqa.selenium.support.FindBy;
-
-public @interface FindAll {
-
-	FindBy[] value();
-
-}
